0ea7ded7dbba1f88504fd7f0432687f7b7b53306
Jim Padilla <jp_code2013@yahoo.com>
2017-02-10 14:24:33 -0800

    Add delete spec

--- spec/features/edit_album_pages_spec.rb
+++ spec/features/edit_album_pages_spec.rb
@@ -18,4 +18,13 @@ describe "the edit an album path" do
     click_button 'Update Album'
     expect(page).to have_content 'errors'
   end
+
+  describe 'the delete mixtape path' do
+    it 'will delete a mixtape' do
+      album = Album.create(name: 'Self-titled', price: 20.5 , artist: 'Guy', release_date: '2017-02-01', image: 'default')
+      visit album_path(album)
+      click_on 'Delete'
+      expect(page).to_not have_content('Self-titled')
+    end
+  end
 end
