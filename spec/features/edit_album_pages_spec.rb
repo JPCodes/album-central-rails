bfdce8eea42dfa286d6db723c66ca863d38e1481
Jim Padilla <jp_code2013@yahoo.com>
2017-02-10 14:19:29 -0800

    Add edit specs for album

---
+++ spec/features/edit_album_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "the edit an album path" do
+  it "edits a album" do
+    album = Album.create(name: 'Self-titled', price: 25.5 , artist: 'Bob', release_date: '2017-01-01', image: 'default')
+    visit album_path(album)
+    click_on 'Edit'
+    fill_in 'Name', :with => 'Untitled'
+    click_on 'Update Album'
+    expect(page).to have_content 'Untitled'
+  end
+
+  it "gives error when no name is entered" do
+    album = Album.create(name: 'Success', price: 20.5 , artist: 'Guy', release_date: '2017-02-01', image: 'default')
+    visit album_path(album)
+    click_on 'Edit'
+    fill_in 'Name', :with => ''
+    click_button 'Update Album'
+    expect(page).to have_content 'errors'
+  end
+end
