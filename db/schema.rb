ede0edf1939aeb811ccf84ddfb249eb1e8b99254
Jim Padilla <jp_code2013@yahoo.com>
2017-02-10 09:06:53 -0800

    Create migration: add image column to albums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210162922) do
+ActiveRecord::Schema.define(version: 20170210170602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170210162922) do
     t.float  "price"
     t.string "artist"
     t.date   "release_date"
+    t.string "image"
   end
 
 end
