6f7edcf110b466a82fa902fb71984c517c19ba22
Jim Padilla <jp_code2013@yahoo.com>
2017-02-10 09:04:04 -0800

    Change root, add album index, new and edit views/controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,23 @@
 class AlbumsController < ApplicationController
   def index
+    @albums = Album.all
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.create(album_params)
+    if @album.save
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+private
+  def album_params
+    params.require(:album).permit(:name, :price, :artist, :release_date)
   end
 end
