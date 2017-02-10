70a7c74f01ebe804b1f1338ed4627de849f2e55e
Jim Padilla <jp_code2013@yahoo.com>
2017-02-10 08:48:24 -0800

    Add first spec, capybara

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,3 @@
 class Album < ActiveRecord::Base
-  
+  validates :name, :price, :artist, :release_date, :presence => true
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 describe Album do
-  
+  it {
+    should validate_presence_of :name
+    should validate_presence_of :price
+    should validate_presence_of :artist
+    should validate_presence_of :release_date
+  }
 end
